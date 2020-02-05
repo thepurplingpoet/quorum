6b8fae616edd1c953f385d7e5ebb9698a18d1066
Gagan <44533738+thepurplingpoet@users.noreply.github.com>
2020-02-05 15:11:46 +0530

    add skeletal home page view

--- quora/knowledge/models.py
+++ quora/knowledge/models.py
@@ -9,6 +9,14 @@ class User(models.Model):
 
     def __str__(self):
         return self.name
+    
+    class Meta: 
+        ordering = ['name']
+
+    def get_absolute_url(self):
+        return reverse('user-detail-view', args=[str(self.id)]) 
+
+
 class Question(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     question_text = models.CharField(max_length=200)
@@ -16,18 +24,43 @@ class Question(models.Model):
 
     def __str__(self):
         return self.question_text
+    
+    class Meta: 
+        ordering = ['-pub_date']
+
+    def get_absolute_url(self):
+        return reverse('question-detail-view', args=[str(self.id)]) 
+
 
 class Answer(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     answer_text = models.TextField()
     upvotes = models.IntegerField(blank=True, null=True, default=0)
+    pub_date = models.DateTimeField('date added')
 
     def __str__(self):
-        return f'Answer by {self.user.name} to {self.question.question_text[:20]} ...'
+        return f'Answer by {self.user.username} to {self.question.question_text[:20]} ...'
+
+    class Meta: 
+        ordering = ['-pub_date']
+
+    def get_absolute_url(self):
+        return reverse('answer-detail-view', args=[str(self.id)]) 
+
 
 class Comment(models.Model):
     answer = models.ForeignKey(Answer, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     comment_text = models.TextField()
+    pub_date = models.DateTimeField('date added')
+
+    def __str__(self):
+        return f'Comment by {self.user.name} on {self.user.username}\'s answer to {self.answer.question.question_text[:20]} ...'
+
+    class Meta: 
+        ordering = ['-pub_date']
+
+    def get_absolute_url(self):
+        return reverse('comment-detail-view', args=[str(self.id)]) 
 
--- quora/knowledge/views.py
+++ quora/knowledge/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from .models import User, Question, Answer, Comment
+
+def index(request):
+    """View function for home page of site."""
+
+    # Generate counts of some of the main objects
+    num_users = User.objects.all().count()
+    num_questions = Question.objects.all().count()
+
+    
+    context = {
+        'num_questions': num_questions,
+        'num_users': num_users
+    }
+
+    # Render the HTML template index.html with the data in the context variable
+    return render(request, 'index.html', context=context)
