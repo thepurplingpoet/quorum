3f012e682e1fd854f56c161f20e3f9e33e08ca53
Gagan <44533738+thepurplingpoet@users.noreply.github.com>
2020-02-05 14:16:54 +0530

    Add basic models

--- quora/knowledge/admin.py
+++ quora/knowledge/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import User, Question, Answer, Comment
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Question)
+admin.site.register(Answer)
+admin.site.register(Comment)
+
--- quora/knowledge/models.py
+++ quora/knowledge/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    name = models.CharField(max_length=100)
+    username = models.CharField(max_length=12)
+    bio = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+class Question(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+    def __str__(self):
+        return self.question_text
+
+class Answer(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    answer_text = models.TextField()
+    upvotes = models.IntegerField(blank=True, null=True, default=0)
+
+    def __str__(self):
+        return f'Answer by {self.user.name} to {self.question.question_text[:20]} ...'
+
+class Comment(models.Model):
+    answer = models.ForeignKey(Answer, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    comment_text = models.TextField()
+
